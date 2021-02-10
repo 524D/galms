c90f0fd83839eb83d56497c50632d87c401745a9
Rob Marissen <50015273+524D@users.noreply.github.com>
2021-02-10 21:47:44 +0100

    Extended FASTA test

--- fasta/fasta_test.go
+++ fasta/fasta_test.go
@@ -1,6 +1,7 @@
 package fasta
 
 import (
+	"bytes"
 	"io"
 	"reflect"
 	"strings"
@@ -21,7 +22,7 @@ func TestRead(t *testing.T) {
 			name: "Simple FASTA",
 			args: args{reader: strings.NewReader(`>BLABLA Something
 ACDEFGH
->BLA2
+>BLA2  
 EEEY
 >
 WE
@@ -100,6 +101,201 @@ HAHAHA
 			}
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("Read() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestFasta_Prots(t *testing.T) {
+	type fields struct {
+		prot []Prot
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   []Prot
+	}{
+		{
+			name: "Simple FASTA prots",
+			fields: fields{
+				prot: []Prot{
+					{id: "BLABLA", desc: "Something", seq: "ACDEFGH"},
+					{id: "BLA2", desc: "", seq: "EEEY"},
+					{id: "DUMMY_ID_3", desc: "", seq: "WEARECPM"},
+				},
+			},
+
+			want: []Prot{
+				{id: "BLABLA", desc: "Something", seq: "ACDEFGH"},
+				{id: "BLA2", desc: "", seq: "EEEY"},
+				{id: "DUMMY_ID_3", desc: "", seq: "WEARECPM"},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			f := &Fasta{
+				prot: tt.fields.prot,
+			}
+			if got := f.Prots(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Fasta.Prots() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestProt_ID(t *testing.T) {
+	type fields struct {
+		id   string
+		desc string
+		seq  string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   string
+	}{
+		{
+			name: "",
+			fields: fields{
+				id:   "SOMEPROT",
+				desc: "Cysteine rich peptide",
+				seq:  "CCCCCCCCCCCCC",
+			},
+			want: "SOMEPROT",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p := &Prot{
+				id:   tt.fields.id,
+				desc: tt.fields.desc,
+				seq:  tt.fields.seq,
+			}
+			if got := p.ID(); got != tt.want {
+				t.Errorf("Prot.ID() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestProt_Description(t *testing.T) {
+	type fields struct {
+		id   string
+		desc string
+		seq  string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   string
+	}{
+		{
+			name: "",
+			fields: fields{
+				id:   "SOMEPROT",
+				desc: "Cysteine rich peptide",
+				seq:  "CCCCCCCCCCCCC",
+			},
+			want: "Cysteine rich peptide",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p := &Prot{
+				id:   tt.fields.id,
+				desc: tt.fields.desc,
+				seq:  tt.fields.seq,
+			}
+			if got := p.Description(); got != tt.want {
+				t.Errorf("Prot.Description() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestProt_Sequence(t *testing.T) {
+	type fields struct {
+		id   string
+		desc string
+		seq  string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   string
+	}{
+		{
+			name: "",
+			fields: fields{
+				id:   "SOMEPROT",
+				desc: "Cysteine rich peptide",
+				seq:  "CCCCCCCCCCCCC",
+			},
+			want: "CCCCCCCCCCCCC",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p := &Prot{
+				id:   tt.fields.id,
+				desc: tt.fields.desc,
+				seq:  tt.fields.seq,
+			}
+			if got := p.Sequence(); got != tt.want {
+				t.Errorf("Prot.Sequence() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestFasta_Write(t *testing.T) {
+	type fields struct {
+		prot []Prot
+	}
+	tests := []struct {
+		name       string
+		fields     fields
+		wantWriter string
+		wantErr    bool
+	}{
+		{
+			name: "Simple test",
+			fields: fields{
+				prot: []Prot{
+					{id: "BLABLA", desc: "Something", seq: "ACDEFGH"},
+				},
+			},
+			wantWriter: ">BLABLA\tSomething\nACDEFGH\n",
+			wantErr:    false,
+		},
+		{
+			name: "Full test",
+			fields: fields{
+				prot: []Prot{
+					{id: "BLABLA", desc: "Something", seq: "ACDEFGH"},
+					{id: "BLA2", desc: "XXX", seq: "ABCDEFGHIJABCDEFGHIJABCDEFGHIJABCDEFGHIJABCDEFGHIJABCDEFGHIJABCDEFGHIJ"},
+					{id: "BLA3", desc: "XXX", seq: "ABCDEFGHIJABCDEFGHIJABCDEFGHIJABCDEFGHIJABCDEFGHIJABCDEFGHIJ"},
+					{id: "BLA4", desc: "ZZZ", seq: "ABCDEFGH"},
+				},
+			},
+			wantWriter: ">BLABLA\tSomething\nACDEFGH\n>BLA2\tXXX\nABCDEFGHIJABCDEFGHIJABCDEFGHIJABCDEFGHIJABCDEFGHIJABCDEFGHIJ\nABCDEFGHIJ\n>BLA3\tXXX\nABCDEFGHIJABCDEFGHIJABCDEFGHIJABCDEFGHIJABCDEFGHIJABCDEFGHIJ\n>BLA4\tZZZ\nABCDEFGH\n",
+			wantErr:    false,
+		},
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			f := &Fasta{
+				prot: tt.fields.prot,
+			}
+			writer := &bytes.Buffer{}
+			if err := f.Write(writer); (err != nil) != tt.wantErr {
+				t.Errorf("Fasta.Write() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if gotWriter := writer.String(); gotWriter != tt.wantWriter {
+				t.Errorf("Fasta.Write() = %v, want %v", gotWriter, tt.wantWriter)
 			}
 		})
 	}
