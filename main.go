e04982b83d5a80d95f2cd7d3306936a30fa99b18
Rob Marissen <50015273+524D@users.noreply.github.com>
2021-05-16 21:07:38 +0200

    Updated copyright message

--- main.go
+++ main.go
@@ -1,3 +1,6 @@
+// Copyright 2021 Rob Marissen
+// SPDX-License-Identifier: MIT
+
 package main
 
 import "github.com/524D/galms/cmd"
--- molecule/molecule_test.go
+++ molecule/molecule_test.go
@@ -1,3 +1,6 @@
+// Copyright 2021 Rob Marissen
+// SPDX-License-Identifier: MIT
+
 package molecule
 
 import (
