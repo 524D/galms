9d7acd3971bd66937d1e5102d7a742633a0428ba
Rob Marissen <50015273+524D@users.noreply.github.com>
2022-06-06 21:58:10 +0200

    Fix test

--- mzidentml/reader_test.go
+++ mzidentml/reader_test.go
@@ -10,10 +10,13 @@ import (
 )
 
 // Test files, downloaded Pride
-const testFile1 = "/home/robm/data/mzid_testfiles/F181955 -filtered.mzid"
-const testFile2 = "/home/robm/data/mzid_testfiles/BalbfedbyRag_Day7_01.mzid"
+// const testFile1 = "/home/robm/data/mzid_testfiles/F181955 -filtered.mzid"
+// const testFile2 = "/home/robm/data/mzid_testfiles/BalbfedbyRag_Day7_01.mzid"
 
-// const testFile = "/home/robm/data/mzid_testfiles/BalbfedbyRag_Day7_01.mzid"
+// Test files, referenced from mzID specification at https://www.psidev.info/mzidentml
+// downloaded from https://github.com/HUPO-PSI/mzIdentML/tree/master/examples/
+const testFile1 = "./testfiles/55merge_tandem.mzid"
+const testFile2 = "./testfiles/combined_1.2.mzid"
 
 func TestAll1(t *testing.T) {
 
@@ -28,15 +31,17 @@ func TestAll1(t *testing.T) {
 	//	log.Printf("%+v\n", f.content)
 
 	n := f.NumIdents()
-	if n != 8600 {
-		t.Errorf("NumIdents is %d, expected 8600", n)
+	if n != 170 {
+		t.Errorf("NumIdents is %d, expected 170", n)
 	}
-	ident, err := f.Ident(1000)
+	ident, err := f.Ident(100)
 	if err != nil {
 		t.Errorf("Ident: error return %v", err)
 	}
-	log.Printf("ident: %+v", ident)
-
+	// log.Printf("ident: %+v", ident)
+	if ident.PepSeq != `FLLSEVGPMSAR` {
+		t.Errorf("Seqence 100=%s, expected FLLSEVGPMSAR", ident.PepSeq)
+	}
 }
 
 func TestAll2(t *testing.T) {
@@ -52,7 +57,7 @@ func TestAll2(t *testing.T) {
 	//	log.Printf("%+v\n", f.content)
 
 	n := f.NumIdents()
-	if n != 8754 {
+	if n != 11707 {
 		t.Errorf("NumIdents is %d, expected 8754", n)
 	}
 	ident, err := f.Ident(1000)
